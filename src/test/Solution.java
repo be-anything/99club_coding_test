eec8edb78142006d59e98f5fa1d40fdee2200670
minjeong <snailmin27@gmail.com>
2024-05-24 23:24:59 +0900

    Feat: 2일차 문제 풀이

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -1,12 +1,37 @@
 package test;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         s.isValid("([]){");
+        s.numberGame(new int[]{2,5});
     }
+
+    public int[] numberGame(int[] nums) {
+        int[] result = new int[nums.length];
+        // 1. 작은 값 순으로 정렬하기
+        for(int i = 0; i < nums.length; i++){
+            int min = nums[i];
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[j] < min){
+                    min = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = min;
+                }
+            }
+        }
+        // 2. 2칸씩 자리바꿔서 저장하기
+        for(int i = 0; i < nums.length; i += 2){
+            int tmp = nums[i + 1];
+            nums[i + 1] = nums[i];
+            nums[i] = tmp;
+        }
+        return result;
+    }
+
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
